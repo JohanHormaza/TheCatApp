e61718ffcc90398d8fe77a097515ced6ed169af0
Johan Hormaza Ricardo <38224658+JohanHormaza@users.noreply.github.com>
2023-05-31 18:06:18 -0500

    Add files via upload

---
+++ App/thecatsapp/build/AppCatProject.build/Release-iphonesimulator/AppCatProject.build/Objects-normal/x86_64/LS_splash.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/ionverse/Desktop/JohanServer_iOS/UploadedProjects/thecatsapp/B4iProject/LS_splash.m \
+  /Users/ionverse/Desktop/JohanServer_iOS/Libs/iCore.h
---
+++ App/thecatsapp/build/AppCatProject.build/Release-iphonesimulator/AppCatProject.build/Objects-normal/x86_64/b4i_httputils2service.d
@@ -0,0 +1,13 @@
+dependencies: \
+  /Users/ionverse/Desktop/JohanServer_iOS/UploadedProjects/thecatsapp/B4iProject/b4i_httputils2service.m \
+  /Users/ionverse/Desktop/JohanServer_iOS/UploadedProjects/thecatsapp/B4iProject/b4i_httputils2service.h \
+  /Users/ionverse/Desktop/JohanServer_iOS/Libs/iCore.h \
+  /Users/ionverse/Desktop/JohanServer_iOS/Libs/iHttp.h \
+  ../../Libs/iCore.h \
+  /Users/ionverse/Desktop/JohanServer_iOS/Libs/iStringUtils.h \
+  /Users/ionverse/Desktop/JohanServer_iOS/Libs/iXUI.h \
+  /Users/ionverse/Desktop/JohanServer_iOS/Libs/JHRListView.h \
+  ../../Libs/iXUI.h /Users/ionverse/Desktop/JohanServer_iOS/Libs/iJSON.h \
+  /Users/ionverse/Desktop/JohanServer_iOS/UploadedProjects/thecatsapp/B4iProject/b4i_main.h \
+  /Users/ionverse/Desktop/JohanServer_iOS/UploadedProjects/thecatsapp/B4iProject/b4i_httpjob.h \
+  /Users/ionverse/Desktop/JohanServer_iOS/UploadedProjects/thecatsapp/B4iProject/b4i_listcatspage.h
